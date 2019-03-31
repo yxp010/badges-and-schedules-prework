4583e6d09afec001c9e56b099fabb7bad06db2b8
harmonious-innovator-3896 <kevin20130209@hotmail.com>
2019-03-31 00:58:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  batch_badge = []
+  attendees.each |attendee| do
+    batch_badge.push(_maker(attendee))
+  end
+end
+
+def assign_rooms(attendees)
+  list = []
+  room_number = 1
+  attendees.each |attendee| do
+    list.push("Hello, #{attendee}! You'll be assigned to room #{room_number}")
+    room_number += 1
+  end
+
+  def printer
+    puts batch_badge_creator(attendees)
+    puts assign_rooms(attendees)
+  end
